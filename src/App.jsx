db46dcf43926d47afac550d0a6f152db74fde066
Mihir <aminvasudev6@gmail.com>
2025-02-09 13:15:56 +0530

    otf files + custom css file changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import Navbar from './components/Navbar';
+
 function App() {
-  return <div className="bg-red-500">Hello</div>;
+  return (
+    <div className="App bg-black text-white">
+      <Navbar />
+      <h1>hackscript 6.0</h1>
+      <p>font checking text</p>
+    </div>
+  );  
 }
+
 export default App;
