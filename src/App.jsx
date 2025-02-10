cea222e7167aaf5ef52598c5a4ce3fbb10055574
manastechnical <manasjagtap20@gmail.com>
2025-02-10 16:18:54 +0530

    Timeline v1 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Timeline from "./components/Timeline";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <Hero />
       <About />
+      <Timeline />
     </div>
   );
 }
