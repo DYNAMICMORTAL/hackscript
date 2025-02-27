7ba2d81cfab67b50fc43fb06f526cb009a7484e7
Mihir <aminvasudev6@gmail.com>
2025-02-27 21:45:46 +0530

    Register Now!!!

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,13 @@ import Prize from "./components/Prize";
 import FAQ from "./components/FAQ";
 import ScrollSpeedIndicator from "./components/ScrollSpeedIndicator";
 import Formula1Footer from "./components/Location";
+// import { SpeedInsights } from "@vercel/speed-insights/next"
+import Marquee from "./components/Marquee";
 
 function App() {
   return (
     <div className="bg-black text-secondary h-screen w-full font-f1">
+      <Marquee />
       <ScrollSpeedIndicator />
       {/* <Sidebar /> */}
       <Navbar />
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Marquee.css';
+
+const Marquee = () => {
+  return (
+    <div className="marquee-container">
+      <marquee>Registrations closing soon! Do register for Hackathon.</marquee>
+    </div>
+  );
+};
+
+export default Marquee;
